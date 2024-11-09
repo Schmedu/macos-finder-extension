c5faaa0c9730421c096c79ecbcd6e9ea2eabdbc8
Eduard Uffelmann <edu3@gmx.net>
2024-11-09 12:21:39 +0100

    add get-file-name, -file-path, -directory-path

---
+++ scripts/get-file-path.ts
@@ -0,0 +1,11 @@
+// Name: Get File Path
+// Description: 
+// Input: 
+// Output: 
+// Tags: 
+// Author: Eduard Uffelmann
+// Twitter: @schmedu_
+// Linkedin: https://www.linkedin.com/in/euffelmann/
+// Website: https://schmedu.com
+
+import "@johnlindquist/kit";
